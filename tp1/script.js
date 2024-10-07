e313af0da61f95d0afd64966b5f5d4b2086d185f
SemaKaraca <karacasema08@gmail.com>
2024-10-07 11:21:20 +0200

    update

---
+++ tp1/script.js
@@ -0,0 +1,14 @@
+var string = "Bonjour, je suis Ingénieur d'étude et développement"/* type your text here */
+var array = string.split("");
+var timer;
+
+function frameLooper () {
+	if (array.length > 0) {
+		document.getElementById("text").innerHTML += array.shift();
+	} else {
+		clearTimeout(timer);
+			}
+	loopTimer = setTimeout('frameLooper()',70); /* change 70 for speed */
+
+}
+frameLooper();
